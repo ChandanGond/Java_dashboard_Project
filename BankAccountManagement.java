098380a485867c1ef545687ba44e059c698540b4
Chandan Gond <125117082+ChandanGond@users.noreply.github.com>
2023-07-27 09:53:10 +0530

    Add files via upload

---
+++ BankAccountManagement.java
@@ -0,0 +1,98 @@
+import java.util.Scanner;
+
+class BankAccount {
+    private String accountNumber;
+    private String accountHolder;
+    private double balance;
+
+    public BankAccount(String accountNumber, String accountHolder, double initialBalance) {
+        this.accountNumber = accountNumber;
+        this.accountHolder = accountHolder;
+        this.balance = initialBalance;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getAccountHolder() {
+        return accountHolder;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+        System.out.println("Deposited: $" + amount);
+    }
+
+    public void withdraw(double amount) {
+        if (balance >= amount) {
+            balance -= amount;
+            System.out.println("Withdrawn: $" + amount);
+        } else {
+            System.out.println("Insufficient funds.");
+        }
+    }
+}
+
+public class BankAccountManagement {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Bank Account Management System");
+        System.out.println("-----------------------------");
+
+        // Create a new bank account
+        System.out.print("Enter Account Number: ");
+        String accountNumber = scanner.nextLine();
+
+        System.out.print("Enter Account Holder Name: ");
+        String accountHolder = scanner.nextLine();
+
+        System.out.print("Enter Initial Balance: ");
+        double initialBalance = scanner.nextDouble();
+        scanner.nextLine();
+
+        BankAccount account = new BankAccount(accountNumber, accountHolder, initialBalance);
+
+        // Show menu options
+        char choice;
+        do {
+            System.out.println("\nSelect an option:");
+            System.out.println("1. Deposit");
+            System.out.println("2. Withdraw");
+            System.out.println("3. Check Balance");
+            System.out.println("4. Exit");
+            System.out.print("Enter your choice: ");
+            choice = scanner.next().charAt(0);
+
+            switch (choice) {
+                case '1':
+                    System.out.print("Enter the amount to deposit: ");
+                    double depositAmount = scanner.nextDouble();
+                    account.deposit(depositAmount);
+                    break;
+                case '2':
+                    System.out.print("Enter the amount to withdraw: ");
+                    double withdrawAmount = scanner.nextDouble();
+                    account.withdraw(withdrawAmount);
+                    break;
+                case '3':
+                    System.out.println("Account Holder: " + account.getAccountHolder());
+                    System.out.println("Account Number: " + account.getAccountNumber());
+                    System.out.println("Balance: $" + account.getBalance());
+                    break;
+                case '4':
+                    System.out.println("Exiting...");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        } while (choice != '4');
+
+        scanner.close();
+    }
+}
